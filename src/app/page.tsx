685875c49f4e1688846dbdffb136a747af21a42b
Aviral Singh <aviralsingh3501@gmail.com>
2024-06-23 03:30:18 +0530

    initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        hello
-      </div>
-      <Button>Click me!</Button>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
+      heelo
+    </div>
     </main>
   );
 }
